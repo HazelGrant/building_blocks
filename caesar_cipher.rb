b732e3a92e3f4eb9727d8ff2c25809e432c27fee
WendyBeth <wendybeth010@gmail.com>
2014-07-11 11:29:21 -0400

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,15 @@
 # ##From Wikipedia:
-
-    # In cryptography, a Caesar cipher, also known as Caesar's cipher,
-    # the shift cipher, Caesar's code or Caesar shift, is one of the 
-    # implest and most widely known encryption techniques. It is a type of
-   # substitution cipher in which each letter in the plaintext is replaced
-    # by a letter some fixed number of positions down the alphabet. For example,
-   # with a left shift of 3, D would be replaced by A, E would become B, and so on.
-   # The method is named after Julius Caesar, who used it in his private correspondence.
-
+#
+# In cryptography, a Caesar cipher, also known as Caesar's cipher,
+# the shift cipher, Caesar's code or Caesar shift, is one of the 
+# implest and most widely known encryption techniques. It is a type of
+# substitution cipher in which each letter in the plaintext is replaced
+# by a letter some fixed number of positions down the alphabet. For example,
+# with a left shift of 3, D would be replaced by A, E would become B, and so on.
+# The method is named after Julius Caesar, who used it in his private correspondence.
+#
 # ##Your task:
-
+#
 # Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
 
 def initialize
